2be62463eea05f4ffa12e7427449d507315fb2a1
Aadi Chandra <adithya.chandra.in@gmail.com>
2022-01-07 17:27:05 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -137,7 +137,7 @@ let footer = $(`
                     <input type="text" name="field1" placeholder="Your Name" required/>
                     <input type="email" name="field2" placeholder="Email Address"  required/>
                     <textarea name="field3" placeholder="Type your Message" required></textarea>
-                  <input type="submit" value="Send" />
+                   <button name="submit">Send</button>
                 </form>
              </div>
           </div>
